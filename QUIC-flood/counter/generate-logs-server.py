5d1c2c750216c9116933c3d841f39da38ebf4d87
nicolle <nicollepf@gmail.com>
2022-12-21 23:35:49 +0000

    wip

--- QUIC-flood/counter/generate-logs-server.py
+++ QUIC-flood/counter/generate-logs-server.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 import psutil
 import argparse
 import json
+import time
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--request-index", type=int, default=1) # client request index to log
     parser.add_argument("-o", "--output-folder", type=str, default="server-logs")
     return parser.parse_args()
 
@@ -17,41 +18,39 @@ def save(filepath, data):
 
 def main():
     args = get_args()
-    idx = args.request_index
     output_folder = args.output_folder
 
-    mem = psutil.virtual_memory()
-    cpu_f = psutil.cpu_freq()
-    cpu_p = psutil.cpu_percent()
-    net = psutil.net_io_counters()
+    for x in range(100):
+        mem = psutil.virtual_memory()
+        cpu_f = psutil.cpu_freq()
+        cpu_p = psutil.cpu_percent()
+        net = psutil.net_io_counters()
 
-    mem_data = {
-        "request_index": idx,
-        "total": mem.total,
-        "available": mem.available,
-        "percent": mem.percent,
-        "used": mem.used,
-        "free": mem.free
-    }
+        mem_data = {
+            "total": mem.total,
+            "available": mem.available,
+            "percent": mem.percent,
+            "used": mem.used,
+            "free": mem.free
+        }
 
-    cpu_data = {
-        "request_index": idx,
-        "percent": cpu_p,
-        "frequency": cpu_f.current,
-        "min_frequency": cpu_f.min,
-        "max_frequency": cpu_f.max
-    }
+        cpu_data = {
+            "percent": cpu_p,
+            "frequency": cpu_f.current,
+            "min_frequency": cpu_f.min,
+            "max_frequency": cpu_f.max
+        }
 
-    net_data = {
-        "request_index": idx,
-        "bytes_recv": net.bytes_recv,
-        "packets_recv": net.packets_recv,
-        "dropin": net.dropin
-    }
+        net_data = {
+            "bytes_recv": net.bytes_recv,
+            "packets_recv": net.packets_recv,
+            "dropin": net.dropin
+        }
 
-    save(f"{output_folder}/server-mem.json", mem_data)
-    save(f"{output_folder}/server-cpu.json", cpu_data)
-    save(f"{output_folder}/server-net.json", net_data)
+        save(f"{output_folder}/server-mem.json", mem_data)
+        save(f"{output_folder}/server-cpu.json", cpu_data)
+        save(f"{output_folder}/server-net.json", net_data)
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
--- QUIC-flood/counter/run.py
+++ QUIC-flood/counter/run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import pexpect
-import psutil
 import argparse
 import subprocess
 import sys
@@ -18,22 +17,21 @@ class MininetProc:
 
     def run_client(self):
         self.proc.expect("mininet> ", timeout=None)
-        self.proc.sendline(f"h1 python3 send.py --ca-certs tests/pycacert.pem https://10.0.2.2:4433/")
-
-    def __run_client(self):
-        self.proc.expect("mininet> ", timeout=None)
         self.proc.sendline(f"h1 python3 send.py --ca-certs tests/pycacert.pem https://10.0.2.2:4433/ &")
 
-    def run_server_logs(self, request_index):
+    def run_server_logs(self, output_folder):
         self.proc.expect("mininet> ", timeout=None)
-        self.proc.sendline(f"h2 python3 generate-logs-server.py -r {request_index}")
+        self.proc.sendline(f"h2 python3 generate-logs-server.py -o {output_folder} &")
 
-    def run_attack(self, workload_size, logs=False):
+    def run_attack(self, workload_size, output_folder, logs=False):
+        if logs:
+            self.run_server_logs(output_folder)
+
         for i in range(workload_size):
-            self.__run_client()
+            self.run_client()
 
-            if logs:
-                self.run_server_logs(i)
+        self.proc.expect("mininet> ", timeout=None)
+        self.proc.sendline(f"h1 python3 send.py --ca-certs tests/pycacert.pem https://10.0.2.2:4433/")
 
     def exit(self):
         self.proc.expect("mininet> ", timeout=None)
@@ -45,18 +43,20 @@ def clean():
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--attack", type=int, default=0)
+    parser.add_argument("-o", "--output-folder", type=str, default="server-logs")
     return parser.parse_args()
 
 def main():
     args = get_args()
     workload_size = args.attack
+    output_folder = args.output_folder
 
     clean()
     mininet_proc = MininetProc()
     mininet_proc.run_server()
 
     if workload_size > 0:
-        mininet_proc.run_attack(workload_size, logs=True)
+        mininet_proc.run_attack(workload_size, output_folder, logs=True)
     else:
         mininet_proc.run_client()
 
