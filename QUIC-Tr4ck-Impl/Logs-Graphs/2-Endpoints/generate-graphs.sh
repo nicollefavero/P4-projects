85c9b166523510d48814a31ba2e96b8304d27607
nicolle <nicollepf@gmail.com>
2023-01-05 15:30:26 +0000

    final vms charts and new scripts

--- QUIC-Tr4ck-Impl/Logs-Graphs/2-Endpoints/generate-graphs.sh
+++ QUIC-Tr4ck-Impl/Logs-Graphs/2-Endpoints/generate-graphs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-python3 ./generate-graphs.py -f -k -o s2-c2-t1_0 -i test_01/s2-c2-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s2-c4-t1_0 -i test_01/s2-c4-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s2-c8-t1_0 -i test_01/s2-c8-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s2-c16-t1_0 -i test_01/s2-c16-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s2-c32-t1_0 -i test_01/s2-c32-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s2-c2-t1_0 -i test_01/s2-c2-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s2-c4-t1_0 -i test_01/s2-c4-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s2-c8-t1_0 -i test_01/s2-c8-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s2-c16-t1_0 -i test_01/s2-c16-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s2-c32-t1_0 -i test_01/s2-c32-t1_0 -d vms-graphs -a vms;
 
-python3 ./generate-graphs.py -f -k -o s4-c2-t1_0 -i test_01/s4-c2-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s4-c4-t1_0 -i test_01/s4-c4-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s4-c8-t1_0 -i test_01/s4-c8-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s4-c16-t1_0 -i test_01/s4-c16-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s4-c32-t1_0 -i test_01/s4-c32-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s4-c2-t1_0 -i test_01/s4-c2-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s4-c4-t1_0 -i test_01/s4-c4-t1_0 -d vms-graphs -a vms;
+python3 ./generate-graphs.py -f -k -o s4-c8-t1_0 -i test_01/s4-c8-t1_0 -d vms-charts -a vms -y 30 70 -x 0 70;
+#python3 ./generate-graphs.py -f -k -o s4-c16-t1_0 -i test_01/s4-c16-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s4-c32-t1_0 -i test_01/s4-c32-t1_0 -d vms-graphs -a vms;
 
-python3 ./generate-graphs.py -f -k -o s8-c2-t1_0 -i test_01/s8-c2-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s8-c4-t1_0 -i test_01/s8-c4-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s8-c8-t1_0 -i test_01/s8-c8-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s8-c2-t1_0 -i test_01/s8-c2-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s8-c4-t1_0 -i test_01/s8-c4-t1_0 -d vms-graphs -a vms;
+python3 ./generate-graphs.py -f -k -o s8-c8-t1_0 -i test_01/s8-c8-t1_0 -d vms-charts -a vms -y 30 70 -x 0 70;
 
-python3 ./generate-graphs.py -f -k -o s16-c2-t1_0 -i test_01/s16-c2-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s16-c4-t1_0 -i test_01/s16-c4-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s16-c8-t1_0 -i test_01/s16-c8-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s16-c2-t1_0 -i test_01/s16-c2-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s16-c4-t1_0 -i test_01/s16-c4-t1_0 -d vms-graphs -a vms;
+python3 ./generate-graphs.py -f -k -o s16-c8-t1_0 -i test_01/s16-c8-t1_0 -d vms-charts -a vms -y 30 70 -x 0 100;
 
-python3 ./generate-graphs.py -f -k -o s32-c2-t1_0 -i test_01/s32-c2-t1_0 -d vms-graphs -a vms;
-python3 ./generate-graphs.py -f -k -o s32-c4-t1_0 -i test_01/s32-c4-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s32-c2-t1_0 -i test_01/s32-c2-t1_0 -d vms-graphs -a vms;
+#python3 ./generate-graphs.py -f -k -o s32-c4-t1_0 -i test_01/s32-c4-t1_0 -d vms-graphs -a vms;
